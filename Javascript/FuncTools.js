ace5cc77b9e8de05c475d3a35e3809b40e020127
张国强 <834205145@qq.com>
2021-01-17 19:21:40 +0800

    优化

--- Javascript/FuncTools.js
+++ Javascript/FuncTools.js
@@ -1,9 +1,9 @@
 "use strict"
 
 // 防抖函数
-function debounce (delay) {
+function debounce (func, delay, ...args) {
     let timer;
-    return (func, ...args) => {
+    return () => {
 
         if (timer) {
             clearTimeout(timer);
@@ -17,15 +17,19 @@ function debounce (delay) {
 };
 
 // 节流函数
-function throttle (delay) {
+function throttle (func, delay, immediate, ...args) {
     let timer;
-    return (func, ...args) => {
+    let ctx = this;
+    return () => {
         if (timer) {
             return false;
+        } else if (immediate) {
+            func.apply(ctx, args);
+            return false;
         }
-
+        
         timer = setTimeout(() => {
-            func(...args);
+            func.apply(ctx, args);
             timer = null;
         }, delay);
     };
@@ -54,18 +58,19 @@ function getDataToPath (data={}, path='', df='') {
 // console.log(formatChar('-{b}-', s))
 
 // test --debounce
+// const s = {a: 123, b: [4, 5, 6]};
 // const delay = 5000;
-// const d = debounce(6000);
+// const d = debounce(console.log, 3000, getDataToPath(s, 'b.1'));
 // setInterval(() => {
 //     console.log('delay---');
-//     d(console.log, getDataToPath(s, 'b.1'));
+//     d();
 // }, delay);
 
 // test --throttle
 // const s = {a: 123, b: [4, 5, 6]};
 // const delay = 1000;
-// const t = throttle(3000)
+// const t = throttle(console.log, 3000, false, getDataToPath(s, 'b.1'))
 // setInterval(() => {
 //     console.log('delay---');
-//     t(console.log, getDataToPath(s, 'b.1'));
+//     t();
 // }, delay);
