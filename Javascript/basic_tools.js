24e97e691570a2ab9a9f72d42d66e262b3f397b7
睡着的忧伤 <834205145@qq.com>
2021-06-08 22:39:07 +0800

    增加XMR对象 请求/响应 的拦截

--- Javascript/basic_tools.js
+++ Javascript/basic_tools.js
@@ -108,8 +108,8 @@ function formatString (sourceData = {}, char = '') {
 function parseQueryString (url) {
     let queryParams = {};
     const [protocol, otherPath] = url.split('://');
-    const [host, pathString, ...args] = otherPath.split('/');
-    const mergePathString = [pathString, ...args].join('/')
+    const [host, pathString, ..._args] = otherPath.split('/');
+    const mergePathString = [pathString, ..._args].join('/');
     const [pathName, queryString, ...args] = mergePathString.split('?');
     const temp = [queryString, ...args].join('?');
     temp.split('&').forEach(item => {
@@ -130,5 +130,74 @@ function parseQueryString (url) {
         port,
         pathName,
         queryParams
+    };
+}
+
+/**
+ * 
+ * @param {Function} useRequest 接收原生open的参数, 不能去修改, 
+ * @param {Function} useResponse 接收一个对象, 有response属性与自定义的config属性, config属性中有请求的url以及postData和请求方式
+ */
+function custXHR (useRequest, useResponse) {
+    const XHR = window.XMLHttpRequest;
+    if (!XHR) {
+        throw new Error('抱歉，您的浏览器不支持XMLHttpResponse进行请求~');
+    }
+    const equally = (...args) => args;
+    useRequest = useRequest || equally;
+    useResponse = useResponse || equally;
+
+    const open = XHR.prototype.open;
+    const send = XHR.prototype.send;
+    let postData;
+    let requestMethod;
+    let requestUrl;
+    XHR.prototype.open = function (
+        method,
+        targetUrl,
+        isAsync = true,
+        username,
+        password
+    ) {
+        useRequest(method, targetUrl, isAsync, username, password);
+        requestMethod = method;
+        requestUrl = targetUrl;
+
+        this.send = function (data) {
+            postData = data || {};
+            return send.call(this, data);
+        };
+
+        this.addEventListener('readystatechange', function (...args) {
+            if (this.readyState == 4) {
+                let responseData;
+                contentType = this.getResponseHeader('content-type');
+                if (contentType && contentType.indexOf('json')) {
+                    try {
+                        responseData = JSON.parse(this.responseText);
+                    } catch (error) {
+                        responseData = this.responseText;
+                    }
+                }
+
+                useResponse({
+                    config: {
+                        method: requestMethod,
+                        url: requestUrl,
+                        data: postData
+                    },
+                    response: responseData
+                });
+            }
+        });
+
+        return open.call(
+            this,
+            method,
+            targetUrl,
+            (isAsync = true),
+            username,
+            password
+        );
     };
 }
